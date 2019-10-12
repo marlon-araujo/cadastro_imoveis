b8ca935b5bbde9e7dc6b65359fc41d693b7f6f97
Marlon Araújo <marlon_mat@hotmail.com>
2019-10-11 22:45:31 -0300

    adicionando estrutura do carregando

--- application/views/01_estrutura/cabecalho.php
+++ application/views/01_estrutura/cabecalho.php
@@ -49,9 +49,71 @@ $email = $this->session->userdata[SS_USUARIO]['login_usu'];
         .table thead th {
             border-top: 1px solid #ebedf2;
         }
+
+        .spinner {
+          --spinner-size: 13;
+          --line-color: #0ebeff;
+          --line-alpha: 0.61;
+          --ring-color: #000000;
+          --ring-alpha: 0.25;
+          --ring-size: 1;
+
+          font-size: calc(var(--spinner-size) * 1em);
+          width: 1em;
+          height: 1em;
+          border-radius: 50%;
+
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin-top: -104px;
+          margin-left: -104px;
+        }
+        .spinner .line {
+          fill: none;
+          stroke: var(--line-color);
+          stroke-width: var(--ring-size);
+          opacity: var(--line-alpha);
+          stroke-linecap: round;
+          transform-origin: 50% 50%;
+          transform: rotate3d(0, 0, 1, 0deg);
+          animation: 
+            2156ms spinner-arc ease-in-out infinite,
+            1829ms spinner-rotate linear infinite;
+        }
+        .spinner .ring {
+          fill: none;
+          stroke: var(--ring-color);
+          stroke-width: var(--ring-size);
+          opacity: var(--ring-alpha);
+        }
+        @keyframes spinner-rotate {
+          to { transform: rotate3d(0, 0, 1, 360deg); }
+        }
+        @keyframes spinner-arc {
+          from { stroke-dasharray: 0 150; stroke-dashoffset: 0; }
+          to { stroke-dasharray: 100 150; stroke-dashoffset: -140; }
+        }
+
+        .carregando{
+          display: none;
+          position: fixed;
+          z-index: 9999999;
+          background: rgba(0,0,0,0.5);
+          top: 0;
+          bottom: 0;
+          right: 0;
+          left: 0;
+        }
     </style>
   </head>
 
+    <div class="carregando">
+      <svg viewBox="0 0 50 50" class="spinner">
+        <circle class="ring" cx="25" cy="25" r="22.5" />
+        <circle class="line" cx="25" cy="25" r="22.5" />
+      </svg>
+    </div>
     <input type="hidden" id="base_url" value="<?= base_url() ?>" />
 
   <body>
